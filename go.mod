a3827704367530b030e1cd4ad99a896a7b47f7a2
Gaurav Panchal <gkp.125@gmail.com>
2025-05-08 19:23:38 +0530

    Refactor Post and User store implementations to include full struct definitions and update Create methods to accept respective structs as parameters.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/murkh/go-social
 
 go 1.24.3
 
-require github.com/go-chi/chi/v5 v5.2.1 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- internal/store/users.go
+++ internal/store/users.go
@@ -5,10 +5,35 @@ import (
 	"database/sql"
 )
 
+type User struct {
+	ID        int64  `json:"id"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Password  string `json:"-"`
+	Posts     []Post `json:"posts"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
+
 type UserStore struct {
 	db *sql.DB
 }
 
-func (s *UserStore) Create(ctx context.Context) error {
+func (s *UserStore) Create(ctx context.Context, user *User) error {
+	query := `
+	INSERT INTO users (name, email, password)
+	VALUES ($1, $2, $3) RETURNING id, created_at, updated_at
+	`
+	err := s.db.QueryRowContext(
+		ctx, query,
+		user.Name,
+		user.Email,
+		user.Password,
+	).Scan(&user.ID, &user.CreatedAt, &user.UpdatedAt)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
