341cf55fb2afa2b7da58f2bc30f7d2a44830c604
Gaurav Panchal <gkp.125@gmail.com>
2025-05-12 12:50:35 +0530

    Refactor configuration structures and implement database connection logic

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"log"
 
+	"github.com/murkh/go-social/internal/db"
 	"github.com/murkh/go-social/internal/env"
 	"github.com/murkh/go-social/internal/store"
 )
 
 func main() {
-	cfg := Config{
+	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://user:admin@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 25),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 25),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
-	store := store.NewStorage(nil)
+	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	store := store.NewStorage(db)
 	app := &application{
 		config: cfg,
 		store:  store,
